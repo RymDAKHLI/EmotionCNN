b3892d61fcb79af8f7d7c0c0032975984b790255
RymDAKHLI <33175542+RymDAKHLI@users.noreply.github.com>
2020-08-08 14:38:51 +0100

    Update Train.py

--- Train.py
+++ Train.py
@@ -1,6 +1,6 @@
 import Model
 net = Model.EmotionNet(layers=[3, 4, 23, 3])
-net.train_model('train', 'model-01', epochs=200, csvout='model-01-csv.csv')
+net.train_model('train','valid', 'model-01', epochs=200, csvout='model-01-csv.csv')
 net.load_checkpoint('best-model-01.pth.tar')
 net.valid_model('train')
 net.test_model_show('test')
